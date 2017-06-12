6bc24e520f6a75517bb727623bec49c6e00390d0
PoojaSundarraj <poojasundarraj23@gmail.com>
2017-06-12 15:05:17 +0530

    Create year.java

---
+++ year.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class year
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%4==0)
+		System.out.println("leap year");
+		else
+		System.out.println("non leap year");
+	}
+}
+	
